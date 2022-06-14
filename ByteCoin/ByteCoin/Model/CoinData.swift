7b1d1da14fc251e23222d6e108838d51c3b73a15
Jayvee Ballesteros <77189142+jayveeballesteros@users.noreply.github.com>
2022-06-14 22:37:54 +0800

    Add files via upload

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Jayvee Ballesteros on 2022/04/23.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
+
